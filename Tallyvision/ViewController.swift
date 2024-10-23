fd9abe96350378bb39e9a42effd43844379ec2a2
ivaanmijic <imijic02@gmail.com>
2024-10-23 08:53:25 +0200

    initial commit

---
+++ Tallyvision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tallyvision
+//
+//  Created by Ivan Mijic on 23. 10. 2024..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
